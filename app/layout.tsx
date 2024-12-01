aa8894dae3c76a45ad23a0c1d0616a1cd27ff935
AayushBharti <iaayushbharti@gmail.com>
2024-12-01 19:22:00 +0530

    added gridPatterns

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,10 @@ import { ThemeProvider } from "@/components/theme-provider"
 
 import "./globals.css"
 
+import GridPattern from "../components/grid-pattern"
 import { Toaster } from "../components/ui/toaster"
 import { cn } from "../lib/utils"
+
 const outfit = Outfit({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -16,12 +18,12 @@ export const metadata: Metadata = {
   creator: "Aayush Bharti",
   keywords:
     "image converter, video converter, audio converter, unlimited image converter, unlimited video converter, unlimited file conversion, multimedia transformation",
-    authors: [
-      {
-        name: "Aayush Bharti",
-        url: "https://aayushbharti.vercel.app",
-      },
-    ],
+  authors: [
+    {
+      name: "Aayush Bharti",
+      url: "https://aayushbharti.vercel.app",
+    },
+  ],
 }
 export const viewport: Viewport = {
   colorScheme: "dark",
@@ -30,19 +32,18 @@ export const viewport: Viewport = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body
-       className={cn(
-        "relative h-full antialiased selection:bg-purple-600/20 selection:text-purple-400",
-        outfit.className
-      )}
+        className={cn(
+          "relative h-full antialiased selection:bg-purple-600/20 selection:text-purple-400",
+          outfit.className
+        )}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Toaster />
+          <GridPattern />
           <div className="mx-auto min-h-screen max-w-4xl pt-28 lg:max-w-6xl lg:pt-32 2xl:max-w-7xl 2xl:pt-40">
             {children}
           </div>
